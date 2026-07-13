38d34a2dc973e0fce634c9612ad858d97f10975d
SwordSaucy <111262829+SwordSaucy@users.noreply.github.com>
2026-07-13 17:30:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-number = float(input("Enter a number: "))
-
-square_root = number ** 0.5
-
-print("The square root of", number, "is", square_root)
+pencilbox = ["pencil", "eraser", "ruler","calulator", "pen"]
+amount = [1,0,4,2,0]
+a = {key:value for key,value in zip(pencilbox,amount)}
+print(a)
+stock = [x for x in pencilbox if a[x] > 0]
+print(stock)
+userinput = str(input("enter the thing you wish to buy!: "))
+if userinput not in pencilbox or a[userinput] == 0:
+    print("not in stock")
+    exit()
--- main1.py
+++ main1.py
@@ -1,15 +1,4 @@
-student_data = {
-"id1": {"name": "Sara", "class": "V", "subject_integration": "english, math, science"},
-"id2": {"name": "David", "class": "V", "subject_integration": "english, math, science"},
-"id3": {"name": "Sara", "class": "V", "subject_integration": "english, math, science"}, # duplicate of id1
-"id4": {"name": "Surya", "class": "V", "subject_integration": "english, math, science"},
-}
-a = []
-b = {}
-for i,j in student_data.items():
-    unique = (j["name"],j["class"],j["subject_integration"])
-    if unique not in a: 
-        a.append(unique)
-        b[i]=j
-print(a)
-print(b)
+prices = [1,2,4,2,4,1,2]
+userinput = int(input("enter a markup amount:"))
+markup = list(map(lambda x: x + userinput, prices))
+print(markup)

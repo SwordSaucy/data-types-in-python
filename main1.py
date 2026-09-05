5b49f9a44258718a35271a7ac9136801700c7f28
SwordSaucy <111262829+SwordSaucy@users.noreply.github.com>
2026-09-05 19:39:41 +0100

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,21 +1,20 @@
-#create 2 classes of any 2 countries in each class have 3 functions (capital language and type)
-class england():
-    def capital(self):
-        print("the capital is london")
-    def language(self):
-        print("the language spoken is english")
-    def type(self):
-        print("the country is developed")
-class china():
-    def language(self):
-        print("the language spoken is mandarin")
-    def type(self):
-        print("the country is developed")
-    def capital(self):
-        print("the capital is Beijing")
-obj1 = england()
-obj2 = china()
-for i in (obj1,obj2):
-    i.language()
-    i.capital()
-    i.type()
+class book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+    def borrow(self):
+        self.is_borrowed = True
+        print("you have borrowed", self.title)
+    def return_book(self):
+        self.is_borrowedborrowed = False
+        print("you have borrowed the book", self.title)
+book1 = book("red riding hood", "james")
+book2 = book("the hobbit", "jack")
+book3 = book("harry potter", "j.k Rowling")
+book1.borrow()
+book2.borrow()
+book3.borrow()
+book1.return_book()
+book2.return_book()
+book3.return_book()

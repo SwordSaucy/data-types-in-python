7bc3128aeba310506f9d484907e0cfe410eac1c6
SwordSaucy <111262829+SwordSaucy@users.noreply.github.com>
2026-09-08 18:46:38 +0100

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,20 +1,10 @@
-class book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
-        self.is_borrowed = False
-    def borrow(self):
-        self.is_borrowed = True
-        print("you have borrowed", self.title)
-    def return_book(self):
-        self.is_borrowedborrowed = False
-        print("you have borrowed the book", self.title)
-book1 = book("red riding hood", "james")
-book2 = book("the hobbit", "jack")
-book3 = book("harry potter", "j.k Rowling")
-book1.borrow()
-book2.borrow()
-book3.borrow()
-book1.return_book()
-book2.return_book()
-book3.return_book()
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400,500))
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+    pygame.display.flip()
